392362dbc7aca0d57eccf2924d03cb9ee461994f
Jazoub2 <122349536+Jazoub2@users.noreply.github.com>
2023-01-19 11:10:52 +0000

    Deploying to gh-pages from @ Jazoub2/jazoub2.github.io@a58e222fe464fe53b5b93385c608d78ef2ff6749 🚀

---
+++ _app/immutable/components/pages/_layout.svelte-fe379821.js
@@ -0,0 +1,13 @@
+import{S as z,i as J,s as P,C as Q,k as n,q as Y,a as H,D as W,l as o,m as i,r as F,h as a,c as N,n as f,E as t,b as w,F as X,G as Z,H as x,f as ee,t as te}from"../../chunks/index-efcbbb00.js";function ae(m){let r,u,b,d,l,v,h,$,p,B,D,k,g,S,C,_,L,M,y,E;const O=m[1].default,s=Q(O,m,m[0],null);return{c(){r=n("link"),u=n("style"),b=Y(`nav {
+      margin-left: 10%;
+      margin-right: 10%;
+      background-color: rgb(177, 175, 169);
+    
+      
+    }`),d=H(),l=n("body"),v=n("nav"),h=n("ul"),$=n("li"),p=n("a"),B=Y("Hme"),D=H(),k=n("li"),g=n("a"),S=Y("Bolg"),C=H(),_=n("main"),L=n("hr"),M=H(),y=n("main"),s&&s.c(),this.h()},l(e){const c=W("svelte-qrqs7g",document.head);r=o(c,"LINK",{rel:!0,href:!0}),u=o(c,"STYLE",{});var G=i(u);b=F(G,`nav {
+      margin-left: 10%;
+      margin-right: 10%;
+      background-color: rgb(177, 175, 169);
+    
+      
+    }`),G.forEach(a),c.forEach(a),d=N(e),l=o(e,"BODY",{class:!0});var A=i(l);v=o(A,"NAV",{class:!0});var K=i(v);h=o(K,"UL",{});var I=i(h);$=o(I,"LI",{});var R=i($);p=o(R,"A",{href:!0});var T=i(p);B=F(T,"Hme"),T.forEach(a),R.forEach(a),D=N(I),k=o(I,"LI",{});var U=i(k);g=o(U,"A",{href:!0});var V=i(g);S=F(V,"Bolg"),V.forEach(a),U.forEach(a),I.forEach(a),K.forEach(a),C=N(A),_=o(A,"MAIN",{class:!0});var q=i(_);L=o(q,"HR",{class:!0}),M=N(q),y=o(q,"MAIN",{class:!0});var j=i(y);s&&s.l(j),j.forEach(a),q.forEach(a),A.forEach(a),this.h()},h(){f(r,"rel","stylesheet"),f(r,"href","/pico.min.css"),f(p,"href","/"),f(g,"href","/devblog"),f(v,"class","svelte-9op2os"),f(L,"class","svelte-9op2os"),f(y,"class","container svelte-9op2os"),f(_,"class","container-fluid svelte-9op2os"),f(l,"class","svelte-9op2os")},m(e,c){t(document.head,r),t(document.head,u),t(u,b),w(e,d,c),w(e,l,c),t(l,v),t(v,h),t(h,$),t($,p),t(p,B),t(h,D),t(h,k),t(k,g),t(g,S),t(l,C),t(l,_),t(_,L),t(_,M),t(_,y),s&&s.m(y,null),E=!0},p(e,[c]){s&&s.p&&(!E||c&1)&&X(s,O,e,e[0],E?x(O,e[0],c,null):Z(e[0]),null)},i(e){E||(ee(s,e),E=!0)},o(e){te(s,e),E=!1},d(e){a(r),a(u),e&&a(d),e&&a(l),s&&s.d(e)}}}function se(m,r,u){let{$$slots:b={},$$scope:d}=r;return m.$$set=l=>{"$$scope"in l&&u(0,d=l.$$scope)},[d,b]}class ne extends z{constructor(r){super(),J(this,r,se,ae,P,{})}}export{ne as default};
